198b5fc51533f158e2c5d7b55e476d2103255ea0
bugcat.17 <jjms0131@gmail.com>
2026-05-19 11:18:09 +0900

    fix:schema.prisma,prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,12 +1,14 @@
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+  prisma: PrismaClient | undefined; // undefined를 추가하면 더 안전합니다.
 };
 
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient();
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+  });
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
