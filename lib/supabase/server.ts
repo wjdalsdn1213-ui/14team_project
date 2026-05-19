b69396449f3ed42161f2ffd695ee5c8a191e43d8
bugcat.17 <jjms0131@gmail.com>
2026-05-19 11:51:19 +0900

    fix:server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -12,12 +12,12 @@ export function createSupabaseServerClient() {
         getAll() {
           return cookieStore.getAll();
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options: any }[]) {
           cookiesToSet.forEach(({ name, value, options }) => {
             cookieStore.set(name, value, options);
           });
         },
       },
-    },
+    }
   );
 }
